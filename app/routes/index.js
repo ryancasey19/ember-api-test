5c521659a3ac6f95acc30d8e69c249576669143d
ryancasey19 <ryancasey19@gmail.com>
2020-03-27 15:20:49 -0400

    displays data from api backend

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,7 @@
 import Route from '@ember/routing/route';
 
 export default class IndexRoute extends Route {
-    //TODO
-    //create a bromileys model
-    //create a bromileys adapter
-    //proxy to express server
-    //display brom JSON's in the index route
+    model(){
+        return this.store.findAll('brom', 1);
+    }
 }
