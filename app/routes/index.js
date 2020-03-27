edd0b412045914a53e309515aab07ea0afcc3f1a
ryancasey19 <ryancasey19@gmail.com>
2020-03-27 11:33:42 -0400

    created todo

---
+++ app/routes/index.js
@@ -0,0 +1,9 @@
+import Route from '@ember/routing/route';
+
+export default class IndexRoute extends Route {
+    //TODO
+    //create a bromileys model
+    //create a bromileys adapter
+    //proxy to express server
+    //display brom JSON's in the index route
+}
